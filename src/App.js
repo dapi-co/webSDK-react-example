d92eef2d245b6becb6e8f653f6c48384fa1bbd8d
KERTU KOSS <kk3609@nyu.edu>
2022-02-17 23:40:21 +0400

    ReactJS example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
 
-function App() {
+let handler; 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDapiReady: false
+    };
+  }
+
+  linkBankAccount = () => {
+    if (this.state.isDapiReady){
+      handler.open()
+    }
+    else{
+      console.log("Dapi not ready, try again")
+    }
+    
+  }
+
+  componentDidMount = () => {
+
+    handler = window.Dapi.create({
+      environment: window.Dapi.environments.sandbox, //or .production
+      appKey: "YOUR_APP_KEY",
+      countries: ["AE"],
+      bundleID: "YOUR_BUNDLE_ID", 
+      clientUserID: "CLIENT_USER_ID", 
+      isCachedEnabled: true,
+      isExperimental: false,
+      clientHeaders: {},
+      clientBody: {},
+      onSuccessfulLogin: (bankAccount) => {
+        let ba = bankAccount; 
+
+        ba.data.getIdentity()
+        .then((response)=>{
+          console.log(response)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+        
+      },
+      onFailedLogin: (err) => {
+        if (err != null) {
+          console.log("Error");
+          console.log(err);
+        } else {
+          console.log("No error");
+        }
+      },
+      onReady: () =>{
+       this.setState({
+         isDapiReady:true
+        })
+      },
+      onExit: () => {
+      console.log("User exited the flow")
+    }
+  });
+
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={this.linkBankAccount}>
+        Link a Bank Account
+        </button>
     </div>
   );
 }
+}
+
 
 export default App;
